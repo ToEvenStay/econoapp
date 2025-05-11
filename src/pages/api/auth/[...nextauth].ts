98bad018a0e6637452c4414d5ae07523a0bd27db
Bryan ToEvenStay <toevenstay@gmail.com>
2025-05-11 22:16:57 +0200

    Ajout google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
 // src/pages/api/auth/[...nextauth].ts
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '../../../lib/prisma';
 
 const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     EmailProvider({
       server: process.env.EMAIL_SERVER!,
       from: process.env.EMAIL_FROM!,
